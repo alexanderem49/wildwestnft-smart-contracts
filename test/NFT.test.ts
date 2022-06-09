59f3f2b902dc9e71dbedec74a9a74daed7f1d343
Karyna Chelpan <cheplan15@gmail.com>
2022-06-09 22:27:27 +0300

    add purchase + cover with tests

--- test/NFT.test.ts
+++ test/NFT.test.ts
@@ -4,6 +4,11 @@ import { parseUnits } from "@ethersproject/units";
 import { NFT__factory } from "../typechain/factories/NFT__factory";
 import { NFT } from "../typechain/NFT";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { BigNumber } from "ethers";
+
+async function incrementNextBlockTimestamp(amount: number): Promise<void> {
+  return ethers.provider.send("evm_increaseTime", [amount]);
+}
 
 describe('NFT contract', () => {
   let nft: NFT;
@@ -11,18 +16,23 @@ describe('NFT contract', () => {
   let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
   let addr3: SignerWithAddress;
+  let fundingWallet: SignerWithAddress;
   let addrs: SignerWithAddress[];
   let deployTx: any;
+
   const name = "Wild West NFT";
   const symbol = "WWN";
   const baseTokenURI = "ipfs://bafkreib7rk44lfgqzt6jfvma4khx6sgag6edmp4d2avt67flk5wueqfjc4";
   const zeroAddress = '0x0000000000000000000000000000000000000000';
 
   beforeEach(async () => {
-    [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3, fundingWallet, ...addrs] = await ethers.getSigners();
 
     const Nft = (await ethers.getContractFactory('NFT')) as NFT__factory;
-    nft = await Nft.deploy(name, symbol, baseTokenURI, [addr3.address]);
+
+    const deadline = Math.round((new Date().getTime() + 6000) / 1000);
+
+    nft = await Nft.deploy(name, symbol, baseTokenURI, fundingWallet.address, deadline, [addr3.address]);
 
     deployTx = await nft.deployed();
   });
@@ -40,34 +50,98 @@ describe('NFT contract', () => {
       expect(symbol).to.be.equal(await nft.symbol());
     })
 
+    it('should set funding wallet', async () => {
+      expect(fundingWallet.address).to.equal(await nft.fundingWallet());
+    })
+
     it('should added to whitelist', async () => {
       expect(await nft.isWhitelisted(addr3.address)).to.equal(true);
       expect(deployTx).to.emit(nft, "AddedToWhitelist").withArgs([addr3.address]);
     })
   });
 
-  describe('mints', async () => {
-    it('mints successfully', async () => {
-      const addr1BalanceBefore = await nft.balanceOf(addr1.address);
+  describe('gets price', async () => {
+    it('gets price for whitelisted user and deadline not finish', async () => {
+      await nft.addWhitelists([addr1.address]);
 
-      const tokenId = await nft.callStatic.mint(addr1.address);
-      const tx = await nft.mint(addr1.address);
+      expect(await nft.isWhitelisted(addr1.address)).to.equal(true);
+      expect(await nft.priceFor(addr1.address)).to.equal(parseUnits("0.4", 18));
+    })
 
-      const tokenCount = await nft.currentTokenId();
-      const tokenUri = await nft.tokenURI(tokenId);
-      const addr1BalanceAfter = await nft.balanceOf(addr1.address);
+    it('gets price for !whitelisted user and deadline not finish', async () => {
+      expect(await nft.isWhitelisted(addr1.address)).to.equal(false);
+      expect(await nft.priceFor(addr1.address)).to.equal(parseUnits("0.4", 18));
+    })
 
-      expect(tokenId).to.be.equal(0);
-      expect(addr1BalanceAfter).to.equal(addr1BalanceBefore.add(tokenCount));
-      expect(baseTokenURI + tokenId + ".json").to.equal(tokenUri);
-      expect(tx).to.emit(nft, "PermanentURI").withArgs(tokenUri, tokenId);
+    it('gets price for whitelisted user and deadline finished', async () => {
+      await incrementNextBlockTimestamp(6001);
+      await ethers.provider.send("evm_mine", []);
+      await nft.addWhitelists([addr1.address]);
+
+      expect(await nft.isWhitelisted(addr1.address)).to.equal(true);
+      expect(await nft.priceFor(addr1.address)).to.equal(parseUnits("0.4", 18));
+    })
+
+    it('gets price for !whitelisted user and deadline finished', async () => {
+      await incrementNextBlockTimestamp(6001);
+      await ethers.provider.send("evm_mine", []);
+
+      expect(await nft.isWhitelisted(addr1.address)).to.equal(false);
+      expect(await nft.priceFor(addr1.address)).to.equal(parseUnits("0.5", 18));
+    })
+  })
+
+  describe('sets funding wallet', async () => {
+    it('sets funding wallet successfully', async () => {
+
+      const fundingWalletBefore = await nft.fundingWallet();
+
+      await nft.setFundingWallet(addr3.address);
+
+      const fundingWalletAfter = await nft.fundingWallet();
+
+      expect(fundingWalletAfter).to.not.equal(fundingWalletBefore);
+      expect(fundingWalletAfter).to.equal(addr3.address);
+    })
+
+    it('rejects setting while zero address', async () => {
+      await expect(nft.setFundingWallet(zeroAddress)).to.be.revertedWith('NFT: wallet is zero address');
+    })
+  })
+
+  describe('buys NFT', async () => {
+    const tokenId = parseUnits("1", 18);
+
+    it('buys NFT successfully', async () => {
+      const addr1BalanceBefore = await ethers.provider.getBalance(addr1.address);
+      const fundingWalletBalanceBefore = await ethers.provider.getBalance(fundingWallet.address);
+
+      const price = await nft.priceFor(addr1.address);
+
+      const tx = await nft.connect(addr1).buy(tokenId, { value: price });
+
+      const minedTx = await tx.wait();
+      const fee = minedTx.gasUsed.mul(minedTx.effectiveGasPrice);
+
+      const addr1BalanceAfter = await ethers.provider.getBalance(addr1.address);
+      const fundingWalletBalanceAfter = await ethers.provider.getBalance(fundingWallet.address);
+
+      expect(addr1BalanceAfter).to.equal(addr1BalanceBefore.sub(price).sub(fee));
+      expect(fundingWalletBalanceAfter).to.equal(fundingWalletBalanceBefore.add(price));
+      expect(tx).to.emit(nft, "Bought").withArgs(tokenId, addr1.address, price);
+    })
+
+    it('rejects buying NFT while invalid value', async () => {
+      await expect(nft.connect(addr1).buy(tokenId, { value: parseUnits("0.2", 18) })).to.be.revertedWith('NFT: invalid value');
     })
   })
 
   describe('gets token URI', async () => {
     it('gets token URI successfully', async () => {
-      const tokenId = await nft.callStatic.mint(addr1.address);
-      await nft.mint(addr1.address);
+      const tokenId = parseUnits("1", 18);
+      const price = await nft.priceFor(addr1.address);
+
+      await nft.connect(addr1).buy(tokenId, { value: price });
 
       expect(baseTokenURI + tokenId + ".json").to.equal(await nft.tokenURI(tokenId));
     })
